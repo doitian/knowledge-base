381f6c2d493b504fe087b9cbfd7fb3869d6a1feb
ian <me@iany.me>
2020-06-13 11:06:46 +0800

    scripts for Windows

---
+++ bin/create-wiki.ps1
@@ -0,0 +1,20 @@
+﻿$title = if ($args.Count -gt 0) {
+  $args -join " "
+} else {
+  Read-Host -Prompt 'title'
+}
+
+$tickler = "§ Tickler/Tickler-$($title.Substring(0, 1).ToUpper() -replace '[^A-Z]', '_')"
+
+$dir = "$tickler/$TITLE"
+$file = "$dir/♯ $TITLE.md"
+
+mkdir -Force "$dir" | Out-Null
+
+if (-Not (Test-Path -LiteralPath $file)) {
+  @(
+    "# $title"
+  ) | Out-File $file
+}
+
+echo "$file"
